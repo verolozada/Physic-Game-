587cf7c5c8852676c9967e79a21521b67e26d54d
Veronica L <verolozada12@gmail.com>
2018-09-04 10:53:25 -0400

    final version of psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,55 @@
-
 //variables
-var j = 0;
-var wins = 0;
-var losses = 0;
-var guessesSoFar = [];
-var guessesLeft = 9;
+        var j = 0;
+        var wins = 0;
+        var losses = 0;
+        var guessesSoFar = [];
+        var guessesLeft = 9;
 
-//variables to hold references
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-var guessesLeftText = document.getElementById("guessesLeft");
-var guessesSoFarText = document.getElementById("guessesSoFar");
+        //"physic" choices
+        var computerChoices = ("abcdefghijklmnopqrstuvwxyz").split("");
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        
+        //variables to hold references
+        var winsText = document.getElementById("wins");
+        var lossesText = document.getElementById("losses");
+        var guessesLeftText = document.getElementById("guessesLeft");
+        var guessesSoFarText = document.getElementById("guessesSoFar");
 
-//"physic" choices
-var computerChoices = ("abcdefghijklmnopqrstuvwxyz").split("");
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        document.onkeyup = function (event) {
 
-document.onkeyup = function (event) {
+            // user letter choice
+            var userChoice = event.key;
+            // fill the array with the letters chose by the user
+            guessesSoFar.push(userChoice);
 
-    if (j < 9) {
+            if (j < 10) {
 
-        // user letter choice
-        var userChoice = event.key;
-        // fill the array with the letters chose by the user
-        guessesSoFar.push(userChoice);
+                //if the user guess the letter 
+                if (userChoice === computerGuess) {
+                    wins++;
+                    //after the first win the game will start over again
+                    guessesLeft = 9;
+                    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                    j = 0;
+                    guessesSoFar = [];
+                } else {
+                    guessesLeft--;
+                    j++;
+                }
+        
+                if (guessesLeft === 0) {
+                    losses++;
+                    //after the first win the game will start over again
+                    guessesLeft = 9;
+                    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                    j = 0;
+                    guessesSoFar = [];
+                }
 
-        //if the user guess the letter 
-        if (userChoice === computerGuess) {
-            wins++;
-        }
-
-        //if wins === 1 , the game will start over again. counting the wins and losses
-
-        //the user will have 9 guesses in total and everytime a letter is chose this # will decrease
-        guessesLeft--;
-
-        if (guessesLeft === 0) {
-            losses++;
-            //the game will start over again.
-        }
-
-        //variable j increase every time the user choose a letter
-        j++;
-
-        //text on the page:
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-        guessesSoFarText.textContent = "Your guesses So Far: " + guessesSoFar;
-    }
-};
+                //text on the page:
+                winsText.textContent = "Wins: " + wins;
+                lossesText.textContent = "Losses: " + losses;
+                guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+                guessesSoFarText.textContent = "Your guesses So Far: " + guessesSoFar;
+            }
+        };        
